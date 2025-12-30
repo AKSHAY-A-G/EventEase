03e0116c594c5b06f01f77125bed88ff01eb62d4
akshaygeorge2772 <akshayag2277@gmail.com>
2025-12-31 01:52:08 +0530

    Force update api url

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// If VITE_API_URL is set (in Netlify), use it. Otherwise use localhost (for your laptop).
+// If VITE_API_URL is set (in Netlify), use it. Otherwise use localhost.
 const backendUrl = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 const api = axios.create({
